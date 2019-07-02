8e0c835d1538587457f64731182fec2b6466d453
Alec Derwent <alecderwent@gmail.com>
2019-07-02 11:18:04 -0400

    Update AllDataProgram.py

--- AllDataProgram.py
+++ AllDataProgram.py
@@ -14,43 +14,45 @@ import collections
 file = uproot.open("ttbar.root")
 events = uproot.open("ttbar.root")["events"]
 
+##--Constants--##
+Length = len(Jet_E)
+tM = np.linspace(1.75, 5.75, num=200)
+zM=np.linspace(5, 55, num=100)
+yM =np.linspace(0,1500, num=250)
+
 ##--DATA--#####################################################################
 
-#Jet Data
+## Jet Data
 NJet = events.array("NJet")
-#Jet_P = events.arrays("Jet_P[xyz]*")
 Jet_Px, Jet_Py, Jet_Pz = events.arrays("Jet_P[xyz]*", outputtype = collections.namedtuple)
 Jet_E = events.array("Jet_E")
 Jet_btag = events.array("Jet_btag")
 Jet_ID = events.array("Jet_ID")
 
-#Muon Data
+## Muon Data
 NMuon = events.array("NMuon")
-#Muon_P = events.arrays("Muon_P[xyz]*", outputtype = collections.namedtuple)
 Muon_Px, Muon_Py, Muon_Pz = events.arrays("Muon_P[xyz]*", outputtype = collections.namedtuple)
 Muon_E = events.array("Muon_E")
 Muon_Charge = events.array("Muon_Charge")
 Muon_Iso = events.array("Jet_ID")
 
-#Electron Data
+## Electron Data
 NElectron = events.array("NElectron")
-#Electron_P = events.arrays("Electron_P[xyz]*", outputtype = collections.namedtuple)
 Electron_Px, Electron_Py, Electron_Pz = events.arrays("Electron_P[xyz]*", outputtype = collections.namedtuple)
 Electron_E = events.array("Electron_E")
 Electron_Charge = events.array("Electron_Charge")
 Electron_Iso = events.array("Electron_Iso")
 
-#Photon Data
+## Photon Data
 NPhoton = events.array("NPhoton")
-#Photon_P = events.arrays("Photon_P[xyz]*", outputtype = collections.namedtuple)
 Photon_Px, Photon_Py, Photon_Pz = events.arrays("Photon_P[xyz]*", outputtype = collections.namedtuple)
 Photon_E = events.array("Photon_E")
 Photon_Iso = events.array("Photon_Iso")
 
-#MET Data
+## MET Data
 MET_Px, MET_Py = events.arrays("MET_p[xy]*", outputtype = collections.namedtuple)
 
-#M Data
+## M Data
 MChadronicBottom_Px, MChadronicBottom_Py, MChadronicBottom_Pz = events.arrays("MChadronicBottom_p[xyz]*", outputtype = collections.namedtuple)
 MCleptonicBottom_Px, MCleptonicBottom_Py, MCleptonicBottom_Pz = events.arrays("MCleptonicBottom_p[xyz]*", outputtype = collections.namedtuple)
 MChadronicWDecayQuark_Px, MChadronicWDecayQuark_Py, MChadronicWDecayQuark_Pz = events.arrays("MChadronicWDecayQuark_p[xyz]*", outputtype = collections.namedtuple)
@@ -63,8 +65,6 @@ triggerIsoMu24 = events.array("triggerIsoMu24")
 EventWeight = events.array("EventWeight")
 
 ###############################################################################
-
-Length = len(Jet_E)
 
 #
 # Analyze 
@@ -126,3 +126,50 @@ for x in range(0, Length):
     PsysZ.append(newPsysZ) 
     newPsys = newPsysX**2 + newPsysY**2 + newPsysZ**2
     Psys.append(newPsys)
+<<<<<<< Updated upstream
+=======
+
+## Velocity Stuff 
+B = []
+for x in range(0, Length):
+    newPx=PsysX[x]
+    newPy=PsysY[x]
+    newPz=PsysZ[x]
+    newEsys=Esys[x]
+    if newEsys[0]!=0:
+        Bx=newPx/newEsys
+        By=newPy/newEsys
+        Bz=newPz/newEsys
+        # Calculate Bsys, B and Gam for the current event
+        #B=momentum/energy
+        Bsq=Bx**2+By**2+Bz**2
+        sqrtB=math.sqrt(Bsq)
+        B.append(sqrtB)
+    
+    
+## Finding Invariant Mass
+InvMass = []
+for x in range(0, Length):
+    if (Esys[x][0]**2 - Psys[x] > 0):
+        if (Esys[x][0] != 0):
+            newInvMass = math.sqrt((Esys[x][0]**2) - Psys[x])
+        else: 
+            newInvMass = 0
+    else:
+        newInvMass = 0
+    InvMass.append(newInvMass)
+
+
+## Histograms
+plt.subplot(1, 3, 1)
+plt.hist(Esys, zM)
+plt.title('Energy of the System')
+
+plt.subplot(1, 3, 2)
+plt.hist(Psys, yM)
+plt.title('Momentum of the System')
+
+plt.subplot(2, 2, 1)
+plt.hist(InvMass, tM)
+plt.title('Invariant Mass')
+>>>>>>> Stashed changes
